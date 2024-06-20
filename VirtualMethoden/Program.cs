06bd54834bb4e1895551e7b539b2782000dde91b
sergejjarosch <sergejjarosch@gmail.com>
2024-06-20 16:04:31 +0200

    add virtual Methoden

---
+++ VirtualMethoden/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace VirtualMethoden;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hund labrador = new Hund();
+        Katze ushi = new Katze();
+        labrador.MakeSound();
+        ushi.MakeSound();
+    }
+}
+
+class Tier
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Ein tierisches Gereusch+++++");
+    }
+}
+
+class Hund : Tier
+{
+    public override void MakeSound()
+    {
+        base.MakeSound();
+        Console.WriteLine("wuff, Wuff");
+    }
+}
+
+class Katze : Tier
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Mijau....");
+    }
+}
