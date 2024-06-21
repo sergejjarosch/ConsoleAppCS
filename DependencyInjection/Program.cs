7531b67cd8a328e781621fb52d165a76d1b96c0c
sergejjarosch <sergejjarosch@gmail.com>
2024-06-21 12:51:26 +0200

    add interfaces

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,36 +1,82 @@
 ﻿
 
-var notificationService = new NotificationService();
+IMessageService emailService = new EmailService();
+
+var notificationService = new NotificationService(emailService);
+
+
 notificationService.Notify();
 
 
 class NotificationService
 {
+    private readonly IMessageService _messageService;
+
+    public NotificationService(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+    
     public void Notify()
     {
-        var emailService = new EmailService();
-        emailService.SendMessage("Nachricht");
+        _messageService.SendMessage("Nachricht");
     }
-
     public void NotifyAll()
     {
-        var emailService = new EmailService();
-        emailService.SendMessage("Nachricht");
+        _messageService.SendMessage("Nachricht");
     }
 }
 
-class EmailService
+interface IMessageService
 {
+    void SendMessage(string message);
+}
+
+class EmailService : IMessageService
+{
+    private readonly ILogger _logger;
+    public EmailService(ILogger logger)
+    {
+        _logger = logger;
+    }
     public void SendMessage(string message)
     {
         Console.WriteLine("E-Mail: " + message);
+        
+        _logger.Log("Nachricht wurde erfolgreich gesendet");
     }
 }
 
-class TelegramService
+class TelegramService : IMessageService
 {
     public void SendMessage(string message)
     {
         Console.WriteLine("Telegram: " + message);
+    }
+}
+
+interface ILogger
+{
+    void Log(string message);
+}
+
+interface IDatabase
+{
+    void Safe();
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public class PostgresDatabsase : IDatabase
+{
+    public void Safe()
+    {
+        Console.WriteLine("Daten wurden gespeichert");
     }
 }
