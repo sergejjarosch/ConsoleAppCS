b8f7a05650728c1ce9f7c554e9b697f7f78113ee
sergejjarosch <sergejjarosch@gmail.com>
2024-06-25 16:30:31 +0200

    OOP Class init

--- MSTutorialsBeginner/Oop.cs
+++ MSTutorialsBeginner/Oop.cs
@@ -1,10 +1,33 @@
+using System.Drawing;
+using System.Xml.Schema;
+
 namespace MSTutorialsBeginner;
 
 public class Oop
 {
     public static void Main(string[] args)
     {
+    }
+    
+}
 
+class car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+
+    public car() 
+    {
+        Make = "Others";
     }
 
+    public car(string make, string model, int year, string color)
+    {
+        Make = make;
+        Model = make;
+        Year = year;
+        Color = color;
+    }
 }
